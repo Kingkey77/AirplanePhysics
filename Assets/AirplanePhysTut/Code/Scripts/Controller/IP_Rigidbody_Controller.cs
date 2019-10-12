d9b33038b70d134bb80475d08f0f014589accf24
Kingkey77 <kieran77walsh@gmail.com>
2019-10-12 13:19:05 +0100

    Added XBox controller

---
+++ Assets/AirplanePhysTut/Code/Scripts/Controller/IP_Rigidbody_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+    [RequireComponent(typeof(Rigidbody))]
+
+    public class IP_Rigidbody_Controller : MonoBehaviour
+    {
+        #region variables
+        private Rigidbody rb;
+        private AudioSource aSource;
+
+
+        #endregion
+
+        #region Builtin Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            aSource = GetComponent<AudioSource>();
+            if (aSource)
+                aSource.playOnAwake = false;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (rb)
+                HandlePhysics();
+                
+        }
+        #endregion
+
+        #region Custom Methods
+
+        protected virtual void HandlePhysics()
+        {
+
+        }
+        #endregion 
+    }
+}
