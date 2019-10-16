170d808e941bba3376c513f67c9c3fe8d9bd63d2
Kingkey77 <kieran77walsh@gmail.com>
2019-10-16 13:44:54 +0100

    Animating Propellor

--- Assets/AirplanePhysTut/Code/Scripts/Engines/IP_Airplane_Engines.cs
+++ Assets/AirplanePhysTut/Code/Scripts/Engines/IP_Airplane_Engines.cs
@@ -4,32 +4,20 @@ using UnityEngine;
 
 namespace IndiePixel
 {
-
-
-
     public class IP_Airplane_Engines : MonoBehaviour
     {
         #region Variables
+        [Header("Engine Properties")]
         public float maxForce = 200f;
         public float maxRPM = 2550f;
+        public AnimationCurve powerCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+        [Header("Propellors")]
+        public Ip_Airplane_Propellors propellor;
+
         #endregion
 
         #region Built In Methods
-        #endregion
-
-        #region Custom Methods
-        public Vector3 CalculateForce (float throttle)
-        {
-            //Debug.Log("CalculateForce");
-            //return Vector3.zero;
-            float finalThrottle = Mathf.Clamp01(throttle);
-            float finalPower = finalThrottle * maxForce;
-            Vector3 finalForce = transform.forward * finalPower;
-            return finalForce;
-
-        }
-        #endregion
-
         // Start is called before the first frame update
         void Start()
         {
@@ -41,5 +29,32 @@ namespace IndiePixel
         {
 
         }
+        #endregion
+
+        #region Custom Methods
+        public Vector3 CalculateForce (float throttle)
+        {
+            //Debug.Log("CalculateForce");
+            //return Vector3.zero;
+            //caluclate Power
+            float finalThrottle = Mathf.Clamp01(throttle);
+            finalThrottle = powerCurve.Evaluate(finalThrottle);
+            float finalPower = finalThrottle * maxForce;
+
+            //Calculate RPM's
+            float currentRPM = finalThrottle * maxRPM;
+            if (propellor)
+            {
+                propellor.HandlePropellor(currentRPM);
+            }
+
+            //Create Force!!!
+            Vector3 finalForce = transform.forward * finalPower;
+            return finalForce;
+
+        }
+        #endregion
+
+       
     }
 }
---
+++ Assets/AirplanePhysTut/Code/Scripts/Propellors/Ip_Airplane_Propellors.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+    public class Ip_Airplane_Propellors : MonoBehaviour
+    {
+        #region Variables
+        #endregion
+
+        #region Built In Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        #endregion
+        #region Custom Methods
+        public void HandlePropellor(float CurrentRPM)
+        {
+            //calculate Degrees per second (DPS)
+            float dps = (CurrentRPM * 360f) / 60f * Time.deltaTime;
+            transform.Rotate(Vector3.forward, dps);
+        }
+        #endregion
+    }
+}
