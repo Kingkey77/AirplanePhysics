f8d4cb13c5fae872a6fd35fdd6c739a7b22d19b3
Kingkey77 <kieran77walsh@gmail.com>
2019-10-19 10:46:54 +0100

    Propellor texture swaps completed

--- Assets/AirplanePhysTut/Code/Scripts/Propellors/Ip_Airplane_Propellors.cs
+++ Assets/AirplanePhysTut/Code/Scripts/Propellors/Ip_Airplane_Propellors.cs
@@ -7,13 +7,26 @@ namespace IndiePixel
     public class Ip_Airplane_Propellors : MonoBehaviour
     {
         #region Variables
+        [Header("Propeller Properties")]
+        public float minQuadRPM = 300f;
+        public float minTextureSwap = 600f;
+        public GameObject mainProp;
+        public GameObject blurredProp;
+
+        [Header("Material Properties")]
+        public Material blurredPropMat;
+        public Texture2D blurLevel01;
+        public Texture2D blurLevel02;
         #endregion
 
         #region Built In Methods
         // Start is called before the first frame update
         void Start()
         {
-
+            if (mainProp && blurredProp)
+            {
+                HandleSwapping(0f);
+            }
         }
 
         // Update is called once per frame
@@ -28,6 +41,36 @@ namespace IndiePixel
             //calculate Degrees per second (DPS)
             float dps = (CurrentRPM * 360f) / 60f * Time.deltaTime;
             transform.Rotate(Vector3.forward, dps);
+            if (mainProp && blurredProp)
+            {
+                HandleSwapping(CurrentRPM);
+            }
+        }
+
+        public void HandleSwapping(float currentRPM)
+        {
+            if (currentRPM > minQuadRPM)
+            {
+                blurredProp.gameObject.SetActive(true);
+                mainProp.gameObject.SetActive(false);
+                if (blurredPropMat && blurLevel01 && blurLevel02)
+                {
+                    if (currentRPM > minTextureSwap)
+                    {
+                        blurredPropMat.SetTexture("_MainTex", blurLevel02);
+                    }
+                    else
+                    {
+                        blurredPropMat.SetTexture("_MainTex", blurLevel01);
+                    }
+
+                }
+            }
+            else
+            {
+                blurredProp.gameObject.SetActive(false);
+                mainProp.gameObject.SetActive(true);
+            }
         }
         #endregion
     }
