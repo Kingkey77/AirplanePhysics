3ed90c40f7587336b51a07118694afc24ed5b3f5
Kingkey77 <kieran77walsh@gmail.com>
2019-10-30 12:00:34 +0000

    Stubbing in Scripts for follow Camera

---
+++ Assets/AirplanePhysTut/Code/Scripts/Cameras/IP_Airplane_Camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace IndiePixel
+{
+    public class IP_Airplane_Camera : IP_Basic_Follow_Camera
+    {
+        protected override void HandleCamera()
+        {
+            base.HandleCamera();
+        }
+    }
+}
---
+++ Assets/AirplanePhysTut/Code/Scripts/Cameras/IP_Basic_Follow_Camera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace IndiePixel
+{
+
+
+    public class IP_Basic_Follow_Camera : MonoBehaviour
+    {
+        #region Variables
+
+        #endregion
+
+        #region Built in Methods
+       
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+        }
+        #endregion
+
+        #region Custom Method
+        protected virtual void HandleCamera()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
